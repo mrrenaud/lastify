9b606599a072b153afdf62102b2c61a52b2db046
Renaud <info@ntwrk.be>
2014-10-07 00:53:59 +0200

    added third step

--- Lastify/Scripts/app/angular/controller.js
+++ Lastify/Scripts/app/angular/controller.js
@@ -12,7 +12,6 @@ var phonecatApp =
             controller('SongListCtrl', function ($scope, $http, cfpLoadingBar) {
 
                 $scope.songs = [];
-
                 $scope.availableSongs = [];
 
                 $scope.step = 1;
@@ -21,6 +20,9 @@ var phonecatApp =
                 $scope.songsPageIndex = 0;
                 $scope.songsPageMaxIndex = function () {
                     return Math.ceil($scope.songs.length / $scope.songsRange);
+                };
+                $scope.availableSongsPageMaxIndex = function () {
+                    return Math.ceil($scope.availableSongs.length / $scope.songsRange);
                 };
                 $scope.songsRangeOptions = [10, 20, 50, 100];
                 $scope.songsRange = $scope.songsRangeOptions[1];
@@ -54,6 +56,11 @@ var phonecatApp =
                     return $scope.songs.slice(begin, begin + $scope.songsRange - 1);
                 };
 
+                $scope.paginatedAvailableSongs = function () {
+                    var begin = $scope.songsRange * $scope.songsPageIndex;
+                    return $scope.availableSongs.slice(begin, begin + $scope.songsRange - 1);
+                };
+
                 $scope.searchIndex = 0;
                 $scope.searchOnSpotify = function () {
                     $scope.setLoading(true);
@@ -66,6 +73,7 @@ var phonecatApp =
                 $scope.searchNextSong = function () {
                     if ($scope.searchIndex >= $scope.songs.length) {
                         $scope.setLoading(false);
+                        $scope.step = 3;
                         return false;
                     }
 
@@ -77,21 +85,21 @@ var phonecatApp =
                             'index': $scope.searchIndex
                         }
                     }).
-                success(function (data, status, headers, config) {
-                    if (data.tracks && data.tracks.items.length) {
-                        $scope.songs[config.params.index].spotifyUri = data.tracks.items[0].uri;
-                        $scope.songs[config.params.index].spotifyPreview = data.tracks.items[0].preview_url;
-                        $scope.songs[config.params.index].isAvailable = true;
-                    }
-                    $scope.searchNextSong();
-                }).
-                error(function (data, status, headers, config) {
-                    if (data.tracks && data.tracks.items.length) {
-                        $scope.songs[config.params.index].isAvailable = false;
-                    }
-                    // called asynchronously if an error occurs
-                    // or server returns response with an error status.
-                });
+                    success(function (data, status, headers, config) {
+                        if (data.tracks && data.tracks.items.length) {
+                            $scope.songs[config.params.index].spotifyUri = data.tracks.items[0].uri;
+                            $scope.songs[config.params.index].spotifyPreview = data.tracks.items[0].preview_url;
+                            $scope.songs[config.params.index].isAvailable = true;
+                            $scope.availableSongs.push($scope.songs[config.params.index]);
+                        }
+                        $scope.searchNextSong();
+                    }).
+                    error(function (data, status, headers, config) {
+                        if (data.tracks && data.tracks.items.length) {
+                            $scope.songs[config.params.index].isAvailable = false;
+                        }
+                        $scope.searchNextSong();
+                    });
 
                     $scope.searchIndex++;
                     return true;
